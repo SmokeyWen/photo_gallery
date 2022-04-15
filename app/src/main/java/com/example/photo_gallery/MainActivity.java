c40b7fafeb47681330b59e0ea5d9d0e0163ad49f
SmokeyWen <835033838wen@gmail.com>
2022-04-15 12:43:27 -0700

    Add scrolling

--- app/src/main/java/com/example/photo_gallery/MainActivity.java
+++ app/src/main/java/com/example/photo_gallery/MainActivity.java
@@ -1,40 +1,34 @@
 package com.example.photo_gallery;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.content.FileProvider;
-import android.content.Intent;
-import android.graphics.BitmapFactory;
-import android.net.Uri;
-import android.os.Bundle;
-import android.os.Environment;
-import android.provider.MediaStore;
-import android.view.View;
-import android.widget.ImageView;
-import java.io.File;
-import java.io.IOException;
-import java.text.SimpleDateFormat;
+import androidx.appcompat.app.AppCompatActivity; import androidx.core.content.FileProvider;
+import android.content.Intent; import android.graphics.BitmapFactory;
+import android.net.Uri; import android.os.Bundle; import android.os.Environment;
+import android.provider.MediaStore; import android.view.View; import android.widget.EditText;
+import android.widget.ImageView; import android.widget.TextView; import java.io.File;
+import java.io.IOException; import java.text.SimpleDateFormat; import java.util.ArrayList;
 import java.util.Date;
-import android.util.Log;
-
 public class MainActivity extends AppCompatActivity {
     static final int REQUEST_IMAGE_CAPTURE = 1;
     String mCurrentPhotoPath;
+    private ArrayList<String> photos = null;
+    private int index = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        photos = findPhotos();
+        if (photos.size() == 0) {
+            displayPhoto(null);
+        } else {
+            displayPhoto(photos.get(index));
+        }
     }
     public void takePhoto(View v) {
-        Log.i("photo", "started");
         Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        Log.i("photo", String.valueOf(takePictureIntent.resolveActivity(getPackageManager())));
-
         if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-            Log.i("photo", "in if");
             File photoFile = null;
             try {
                 photoFile = createImageFile();
             } catch (IOException ex) {
-                Log.i("photo", "error");
                 // Error occurred while creating the File
             }
             // Continue only if the File was successfully created
@@ -45,10 +39,54 @@ public class MainActivity extends AppCompatActivity {
             }
         }
     }
+    private ArrayList<String> findPhotos() {
+        File file = new File(Environment.getExternalStorageDirectory()
+                .getAbsolutePath(), "/Android/data/com.example.photo_gallery/files/Pictures");
+        ArrayList<String> photos = new ArrayList<String>();
+        File[] fList = file.listFiles();
+        if (fList != null) {
+            for (File f : fList) {
+                photos.add(f.getPath());
+            }
+        }
+        return photos;
+    }
+    public void scrollPhotos(View v) {
+        switch (v.getId()) {
+            case R.id.btnPrev:
+                if (index > 0) {
+                    index--;
+                }
+                break;
+            case R.id.btnNext:
+                if (index < (photos.size() - 1)) {
+                index++;
+            }
+            break;
+            default:
+                break;
+        }
+        displayPhoto(photos.get(index));
+    }
+    private void displayPhoto(String path) {
+        ImageView iv = (ImageView) findViewById(R.id.ivGallery);
+        TextView tv = (TextView) findViewById(R.id.tvTimestamp);
+        EditText et = (EditText) findViewById(R.id.etCaption);
+        if (path == null || path =="") {
+            iv.setImageResource(R.mipmap.ic_launcher);
+            et.setText("");
+            tv.setText("");
+        } else {
+            iv.setImageBitmap(BitmapFactory.decodeFile(path));
+            String[] attr = path.split("_");
+            et.setText(attr[1]);
+            tv.setText(attr[2]);
+        }
+    }
     private File createImageFile() throws IOException {
         // Create an image file name
         String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
-        String imageFileName = "JPEG_" + timeStamp + "_";
+        String imageFileName = "_caption_" + timeStamp + "_";
         File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
         File image = File.createTempFile(imageFileName, ".jpg",storageDir);
         mCurrentPhotoPath = image.getAbsolutePath();
@@ -58,8 +96,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
-            ImageView mImageView = (ImageView) findViewById(R.id.imageView);
+            ImageView mImageView = (ImageView) findViewById(R.id.ivGallery);
             mImageView.setImageBitmap(BitmapFactory.decodeFile(mCurrentPhotoPath));
+            photos = findPhotos();
         }
     }
 }
