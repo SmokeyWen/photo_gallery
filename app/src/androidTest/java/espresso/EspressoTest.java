a4917fb7617c80c6a00fd84bd28423b9167cb584
SmokeyWen <835033838wen@gmail.com>
2022-04-16 17:05:44 -0700

    Add test case that checks navigation and checks search with given timestamp

--- app/src/androidTest/java/espresso/EspressoTest.java
+++ app/src/androidTest/java/espresso/EspressoTest.java
@@ -1,5 +1,7 @@
 package espresso;
 
+import static androidx.test.espresso.action.ViewActions.replaceText;
+import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.matcher.ViewMatchers.*;
 
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
@@ -8,6 +10,11 @@ import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.Espresso.onView;
 import com.example.photo_gallery.R;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import android.view.View;
+
+import android.widget.TextView;
+
+import androidx.test.espresso.Espresso;
 
 
 import com.example.photo_gallery.MainActivity;
@@ -26,5 +33,31 @@ public class EspressoTest {
     public void selectSearchBtn() {
         onView(withText("search")).perform(click());
         onView(withId(R.id.SearchActivity)).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void selectADateRange() {
+        String fromTimeStamp = "2022-04-16 15:40:49";
+        String toTimeStamp = "2022-04-16 15:40:51";
+        String correctTimeStamp = "2022-04-16 15:40:50";
+
+//      Go to search screen
+        onView(withText("search")).perform(click());
+        onView(withId(R.id.SearchActivity)).check(matches(isDisplayed()));
+
+//      Enter the desired timestamp and verify
+        onView(withId(R.id.etFromDateTime)).perform(click()).perform(replaceText(fromTimeStamp));
+        Espresso.closeSoftKeyboard();
+        onView(withId(R.id.etFromDateTime)).check(matches(withText(fromTimeStamp)));
+        onView(withId(R.id.etToDateTime)).perform(click()).perform(replaceText(toTimeStamp));
+        Espresso.closeSoftKeyboard();
+        onView(withId(R.id.etToDateTime)).check(matches(withText(toTimeStamp)));
+
+//      Go back to the main screen and verify
+        onView(withId(R.id.go)).perform(click());
+        onView(withId(R.id.MainActivity)).check(matches(isDisplayed()));
+
+//      Check the timstamp on main screen has the correct timestamp
+        onView(withId(R.id.tvTimestamp)).check(matches(withText(correctTimeStamp)));
     }
 }
